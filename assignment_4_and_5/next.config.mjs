2dafb5f28a8cf56a566bff39ba0e94e3052bf1d9
Muhammad Asif <70218962+AloneAsif@users.noreply.github.com>
2024-10-11 17:24:31 +0500

    Add files via upload

---
+++ assignment_4_and_5/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // next.config.js
+
+    images: {
+      domains: ['m.media-amazon.com'],
+    },
+
+  
+};
+
+export default nextConfig;
